ff90e368e0793bd542b352b3e56a01607014cf1f
Ocean Deep <cloud@oceandeep.com.br>
2020-12-14 10:55:22 -0300

    NodeJs to Python

--- smorcl.py
+++ smorcl.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Oracle:
     def __init__(self, username, password, ip_address, port, service_name):
         self.__username = username
@@ -9,7 +10,7 @@ class Oracle:
         self.__service_name = service_name
         self.__erro = ""
 
-    def exec(self, command):
+    def __exec(self, command):
         stdout = os.popen(command).read()
         stdout = str(stdout).replace("\t", " ").split("\n")
         output = []
@@ -18,7 +19,7 @@ class Oracle:
                 output.append(i)
         return output
 
-    def fetchSeparetedKeyAndValue(self, data):
+    def __fetchSeparetedKeyAndValue(self, data):
         sKey = []
         sValues = []
 
@@ -29,7 +30,7 @@ class Oracle:
 
         return sKey, sValues
 
-    def objToArrayWithComparisionOfAny(self, data, arrow = None):
+    def __objToArrayWithComparisionOfAny(self, data, arrow = None):
         caracter = "="
         if arrow: caracter = arrow
         obj = []
@@ -40,7 +41,7 @@ class Oracle:
             idx += 1
         return obj
 
-    def checkIrregularity(self, output):
+    def __checkIrregularity(self, output):
         if not output:
             return {"status": 404, "data": [], "error": "Not data found"}
         else:
@@ -52,11 +53,11 @@ class Oracle:
                 if 'no rows selected' in i:
                     return {"status": 404, "data": [], "error": i}
 
-    def sqlplus(self, sql):
+    def __sqlplus(self, sql):
         tns_connect = f'{self.__username}/{self.__password}@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST={self.__ip_address})(PORT={self.__port})))(CONNECT_DATA=(SERVICE_NAME={self.__service_name})))'
         sqlplus = f'export NLS_LANG=AMERICAN_AMERICA.UTF8 \n sqlplus -s "{tns_connect}" <<EOF \n set long 30000 \n set longchunksize 30000 \n set pages 0 \n set lines 2000 \n {sql} \nEOF'
-        response = self.exec(sqlplus)
-        existIrregularity = self.checkIrregularity(response)
+        response = self.__exec(sqlplus)
+        existIrregularity = self.__checkIrregularity(response)
         if existIrregularity is None or existIrregularity["error"] is None:
             return {"status": 200, "data": response, "error": self.__erro}
         else:
@@ -64,22 +65,22 @@ class Oracle:
 
 
     def query(self, query):
-        return self.sqlplus(sql=query)
+        return self.__sqlplus(sql=query)
 
     def keepAliveDb(self):
-        response = self.sqlplus('select 1 from dual;')
+        response = self.__sqlplus('select 1 from dual;')
         if response["error"]:
             return {"status": 0, "output": "Failed to connect"}
         else:
             return {"status": 1, "output": "Successfully connected"}
 
     def insert(self, table, data):
-        sKey, sValues = self.fetchSeparetedKeyAndValue(data)
+        sKey, sValues = self.__fetchSeparetedKeyAndValue(data)
         key = str(sKey).strip('[]').replace("'", '')
         vlr = str(sValues).strip('[]')
 
         query = f"insert into {table} ( {key} ) values ( {vlr} );"
-        res = self.sqlplus(query)
+        res = self.__sqlplus(query)
         return res
 
     def insertSelect(self, tablePrimary, columnsPrimary, tableSource, columnsSource, where = None, handsFreeWhere = None):
@@ -87,7 +88,7 @@ class Oracle:
         objWhere = []
 
         if where:
-            objWhere = self.objToArrayWithComparisionOfAny(where)
+            objWhere = self.__objToArrayWithComparisionOfAny(where)
             recort = str(objWhere).strip("[]").replace('"', "").replace(',', ' and ')
             isExistWhere = f" where {recort} ;"
         elif handsFreeWhere:
@@ -96,7 +97,8 @@ class Oracle:
         keyColumnsPrimary = str(columnsPrimary).strip('[]').replace("'", '')
         keyColumnsSource = str(columnsSource).strip('[]').replace("'", '')
         query = f"insert into {tablePrimary} ( {keyColumnsPrimary} ) select {keyColumnsSource} from {tableSource} {isExistWhere}"
-        print(query)
+        res = self.__sqlplus(query)
+        return res
 
     def select(self, table, columns, where = None, handsFreeWhere = None):
         isExistWhere = ";"
@@ -113,14 +115,14 @@ class Oracle:
         col = str(col).strip("[]").strip("'\"").replace('"', '').replace(",", "")
 
         if where:
-            objWhere = self.objToArrayWithComparisionOfAny(where)
+            objWhere = self.__objToArrayWithComparisionOfAny(where)
             recort = str(objWhere).strip("[]").replace('"', "").replace(',', ' and ')
             isExistWhere = f" where {recort} ;"
         elif handsFreeWhere:
             isExistWhere = f" where {handsFreeWhere} ;"
 
         query = f'select {col} from {table} {isExistWhere}'
-        res = self.sqlplus(query)
+        res = self.__sqlplus(query)
 
         objList = []
         if res["data"] and res["error"] == '':
@@ -140,7 +142,7 @@ class Oracle:
 
     def fetchColumnsTable(self, table):
         query = f"select lower(COLUMN_NAME) as COLUMN_NAME from user_tab_columns where table_name = upper('{table}');"
-        res = self.sqlplus(query)
+        res = self.__sqlplus(query)
         arry = []
         for col in res["data"]:
             if 'rows selected' in col:
@@ -149,20 +151,20 @@ class Oracle:
         return arry
 
     def exec_procedure(self, procedure_name, data):
-        vlr = str(self.objToArrayWithComparisionOfAny(data, '=>')).strip("[]").replace('"', "")
+        vlr = str(self.__objToArrayWithComparisionOfAny(data, '=>')).strip("[]").replace('"', "")
         query = f"begin \n {procedure_name}({vlr}); \n end; \n/"
-        res = self.sqlplus(query)
+        res = self.__sqlplus(query)
         return res
 
     def exec_function(self, function_name, data):
-        vlr = str(self.objToArrayWithComparisionOfAny(data, '=>')).strip("[]").replace('"', "")
+        vlr = str(self.__objToArrayWithComparisionOfAny(data, '=>')).strip("[]").replace('"', "")
         query = f"select {function_name}({vlr}) as response from dual;"
-        res = self.sqlplus(query)
+        res = self.__sqlplus(query)
         return res
 
     def truncate(self, table):
         query = f'TRUNCATE TABLE {table};'
-        res = self.sqlplus(query)
+        res = self.__sqlplus(query)
         return res
 
     def drop_table(self, table, casc = None):
@@ -170,5 +172,5 @@ class Oracle:
         if casc:
             cascConst = "CASCADE CONSTRAINTS"
         query = f"DROP TABLE {table} {cascConst}"
-        res = self.sqlplus(query)
+        res = self.__sqlplus(query)
         return res
